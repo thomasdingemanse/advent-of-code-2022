24ad16a969f92e066caf3c9ef207637f6112574f
Thomas Dingemanse <thomas.dingemanse@gmail.com>
2022-12-01 12:59:57 +0100

    Add day 1, part 1

---
+++ 1/main.py
@@ -0,0 +1,16 @@
+with open("./input.txt", "r") as file:
+    most_calories = 0
+    current_calories = 0
+
+    for line in file.readlines():
+        # Add calories to the current elf's calorie count
+        if line != '\n':
+            current_calories += int(line)
+                    
+        # Empty line, start counting again
+        else:
+            if current_calories > most_calories:
+                most_calories = current_calories
+            current_calories = 0
+    
+    print(most_calories)
